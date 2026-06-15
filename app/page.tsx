d6b114647c63ea495d610359f77781401bf734bd
xera <shairafathiya@gmail.com>
2026-06-15 22:55:12 +0700

    add supabse

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,24 @@
 import { useState, useEffect, useCallback } from "react";
 import { Note, CreateNoteDto, UpdateNoteDto } from "@/types/note";
 
+import { createClient } from '@/utils/supabase/server'
+import { cookies } from 'next/headers'
+
+// export default async function Page() {
+//   const cookieStore = await cookies()
+//   const supabase = createClient(cookieStore)
+
+//   const { data: todos } = await supabase.from('todos').select()
+
+//   return (
+//     <ul>
+//       {todos?.map((todo) => (
+//         <li key={todo.id}>{todo.name}</li>
+//       ))}
+//     </ul>
+//   )
+// }
+
 const NOTE_COLORS = [
   "#FFFFFF", "#FFF9C4", "#C8E6C9", "#BBDEFB",
   "#F8BBD0", "#E1BEE7", "#FFE0B2", "#D7CCC8",
